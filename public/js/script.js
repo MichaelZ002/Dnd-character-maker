6babee24aa8fc1059c24204cd0588a761260ca50
Michael Zaslavsky <michaelzaslavsky@Michaels-MacBook-Pro.local>
2020-08-15 11:29:34 -0500

    added basic ajax call

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-    
-
+    const queryURL = "https://www.dnd5eapi.co/api/";
+    $.ajax({ 
+        url: queryURL,
+        type: "GET",
+        success: function (result){
+            console.log(result)
+        },
+        error: function(error){
+            console.log(error);
+        }
+    }) 
 })
