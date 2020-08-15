ee97de3a22e5a26e65db8bab5af4fbf3281dd8fd
Ben de Garcia <degarcia.ben@gmail.com>
2020-08-15 17:30:44 -0500

    added route for / character

--- controllers/dnd_controller.js
+++ controllers/dnd_controller.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require("../models");
 
 router.get("/", (req, res) => {
     res.render('index');
 });
 
-
+router.get("/character/:name", (req, res) => {
+    let id = req.param.name
+    const found = db.character.find(character => character.name === targetChar)
+    res.render("character", found )
+})
 module.exports = router
