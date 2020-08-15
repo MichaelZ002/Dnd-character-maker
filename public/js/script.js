ffd292a1f631018b280ea72ab8fe48b00dc8af89
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-08-15 13:45:12 -0500

    recent changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const queryURL = "https://www.dnd5eapi.co/api/";
-    $.ajax({ 
+    $.ajax({
         url: queryURL,
         type: "GET",
-        success: function (result){
+        success: function (result) {
             console.log(result)
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
-    }) 
+    })
     //for spells
     const spellSelect = $("#spells")
-    spellSelect.on('change',function(event){
-        const spellname = $(this).val() 
+    spellSelect.on('change', function (event) {
+        const spellname = $(this).val()
         console.log(spellname)
-    $.ajax({ 
-        url: queryURL + "spells/" + spellname,
-        type: "GET",
-        success: function (result){
-            console.log(result)
-        },
-        error: function(error){
-            console.log(error);
-        }
-    }) 
+        $.ajax({
+            url: queryURL + "spells/" + spellname,
+            type: "GET",
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
     })
-  
+
 })
