de676957fa110206089030b36b073dffaa470ff8
Michael Zaslavsky <michaelzaslavsky@Michaels-MacBook-Pro.local>
2020-08-15 10:48:03 -0500

    established server connection and database

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8000;
 const exphbs = require("express-handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const routes = require('./controllers/dnd_controllers');
+const routes = require('./controllers/dnd_controller');
 app.use(routes);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
