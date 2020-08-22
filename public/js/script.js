bcbb19e530a99a75f341919303e98c58e81e2134
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-08-21 21:54:21 -0500

    pushing chnages before pulling from master

--- public/js/script.js
+++ public/js/script.js
@@ -12,67 +12,67 @@ $(document).ready(function () {
     })
     //for class
     const classSelect = $("#class")
-    classSelect.on('click',function(event){
-        const className = $(this).val() 
+    classSelect.on('click', function (event) {
+        const className = $(this).val()
         console.log(className)
-    $.ajax({ 
-        url: queryURL + "classes/",
-        type: "GET",
-        success: function (result){
-            console.log(result.results)
-        },
-        error: function(error){
-            console.log(error);
-        }
-    }) 
-    }) 
+        $.ajax({
+            url: queryURL + "classes/",
+            type: "GET",
+            success: function (result) {
+                console.log(result.results)
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    })
     //for spells
     const spellSelect = $("#spells")
-    spellSelect.on('change',function(event){
-        const spellName = $(this).val() 
+    spellSelect.on('change', function (event) {
+        const spellName = $(this).val()
         console.log(spellName)
-    $.ajax({ 
-        url: queryURL + "spells/",
-        type: "GET",
-        success: function (result){
-            console.log(result)
-        },
-        error: function(error){
-            console.log(error);
-        }
-    }) 
+        $.ajax({
+            url: queryURL + "spells/",
+            type: "GET",
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
     })
     //for races
-    const raceSelect = $("#race")   
-    raceSelect.on('change',function(event){
-        const raceName = $(this).val() 
+    const raceSelect = $("#race")
+    raceSelect.on('change', function (event) {
+        const raceName = $(this).val()
         console.log(raceName)
-    $.ajax({ 
-        url: queryURL + "race/",
-        type: "GET",
-        success: function (result){
-            console.log(result)
-        },
-        error: function(error){
-            console.log(error);
-        }
-    }) 
+        $.ajax({
+            url: queryURL + "race/",
+            type: "GET",
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
     })
     //to select languages
     const langSelect = $("#lang")
-    langSelect.on('change',function(event){
-        const langName = $(this).val() 
+    langSelect.on('change', function (event) {
+        const langName = $(this).val()
         console.log(spellname)
-    $.ajax({ 
-        url: queryURL + "languages/",
-        type: "GET",
-        success: function (result){
-            console.log(result)
-        },
-        error: function(error){
-            console.log(error);
-        }
-    }) 
+        $.ajax({
+            url: queryURL + "languages/",
+            type: "GET",
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
     })
 
 })
