ef6e4ec1bcca8ad70f1b162ad62b1d6e29687437
Ben de Garcia <degarcia.ben@gmail.com>
2020-09-01 20:03:31 -0500

    aded compression

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const exphbs = require("express-handlebars");
 const db = require("./models");
+const compression = require('compression')
+const routes = require("./controllers/dnd_controller");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -10,11 +12,12 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(compression())
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-const routes = require("./controllers/dnd_controller");
+
 app.use(routes);
 app.use(express.static("public"));
 
